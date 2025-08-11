2660147d33b169662572ade603a93fe1f1c4d9d9
Kye Morton <pryre.dev@outlook.com>
2025-08-11 17:38:59 +1000

    Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ariel-care-occupancy-map/',
+  // base: '/ariel-care-occupancy-map/',
   plugins: [
     vue(),
     vueDevTools(),
